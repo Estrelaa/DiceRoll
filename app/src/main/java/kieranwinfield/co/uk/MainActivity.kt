04a771a89bf185f948ae9a60ae2b32a012663a24
Kieran Winfield <kieran.winfield464@gmail.com>
2019-07-23 09:29:28 +0100

    Added advantage rolling, now can't roll zeros and can roll the die max value

--- app/src/main/java/kieranwinfield/co/uk/MainActivity.kt
+++ app/src/main/java/kieranwinfield/co/uk/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -17,43 +16,63 @@ class MainActivity : AppCompatActivity() {
         val resultTextView = findViewById<TextView>(R.id.ResultsTextView)
         val numberOfTimesSeekBar = findViewById<SeekBar>(R.id.NumberOfRollsSeekBar)
         val DieType = findViewById<SeekBar>(R.id.DiceYouAreRollingSeekBar)
-        var x = 0
 
         //Roll when the Button is pressed
-        rollButton.setOnClickListener() {
-
-                //D4
-                if (DieType.progress == 0) {
-                    val rand = Random.nextInt(4)
-                    resultTextView.text = rand.toString()
-                    if (numberOfTimesSeekBar.progress == 1){
-                        resultTextView.text = rand.toString()
-                    }
+        rollButton.setOnClickListener{
+            //D4
+            if (DieType.progress == 0) {
+                val rand1 = Random.nextInt(1,5)
+                resultTextView.text = rand1.toString()
+                if (numberOfTimesSeekBar.progress == 1) {
+                    val rand2 = Random.nextInt(1, 5)
+                    resultTextView.text = rand1.toString() + ',' + rand2.toString()
                 }
-                //D6
-                if (DieType.progress == 1) {
-                    val rand = Random.nextInt(6)
-                    resultTextView.text = rand.toString()
+            }
+            //D6
+            if (DieType.progress == 1) {
+                val rand1 = Random.nextInt(1,7)
+                resultTextView.text = rand1.toString()
+                if (numberOfTimesSeekBar.progress == 1) {
+                    val rand2 = Random.nextInt(1, 7)
+                    resultTextView.text = rand1.toString() + ',' + rand2.toString()
                 }
-                //D8
-                if (DieType.progress == 2) {
-                    val rand = Random.nextInt(8)
-                    resultTextView.text = rand.toString()
+            }
+            //D8
+            if (DieType.progress == 2) {
+                val rand1 = Random.nextInt(1,9)
+                resultTextView.text = rand1.toString()
+                if (numberOfTimesSeekBar.progress == 1) {
+                    val rand2 = Random.nextInt(1, 9)
+                    resultTextView.text = rand1.toString() + ',' + rand2.toString()
                 }
-                //D10
-                if (DieType.progress == 3) {
-                    val rand = Random.nextInt(10)
-                    resultTextView.text = rand.toString()
+            }
+            //D10
+            if (DieType.progress == 3) {
+                val rand1 = Random.nextInt(1,11)
+                resultTextView.text = rand1.toString()
+                if (numberOfTimesSeekBar.progress == 1) {
+                    val rand2 = Random.nextInt(1, 11)
+                    resultTextView.text = rand1.toString() + ',' + rand2.toString()
                 }
-                //D12
-                if (DieType.progress == 4) {
-                    val rand = Random.nextInt(12)
-                    resultTextView.text = rand.toString()
+            }
+            //D12
+            if (DieType.progress == 4) {
+                val rand1 = Random.nextInt(1, 13)
+                resultTextView.text = rand1.toString()
+                if (numberOfTimesSeekBar.progress == 1) {
+                    val rand2 = Random.nextInt(1, 13)
+                    resultTextView.text = rand1.toString() + ',' + rand2.toString()
                 }
-                //D20
-                if (DieType.progress == 5) {
-                    val rand = Random.nextInt(20)
-                    resultTextView.text = rand.toString()
+            }
+            //D20
+            if (DieType.progress == 5) {
+                val rand1 = Random.nextInt(1,21)
+                resultTextView.text = rand1.toString()
+                if (numberOfTimesSeekBar.progress == 1) {
+                    val rand2 = Random.nextInt(1, 21)
+                    resultTextView.text = rand1.toString() + ',' + rand2.toString()
                 }
+            }
         }
     }
+}
