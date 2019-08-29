611d4e9073cc1a233195119b164e00ac7fd407a9
Kieran Winfield <kieran.winfield464@gmail.com>
2019-08-29 19:51:53 +0100

    Added another activtiy for picking a race for the random name generator. Fixed the Navigation bar at the bottom on phones

---
+++ app/src/main/java/kieranwinfield/co/uk/SelectRaceActivity.kt
@@ -0,0 +1,14 @@
+package kieranwinfield.co.uk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class SelectRaceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_race)
+    }
+}
